bf4249bbb17c97f1f159082de960671797835d3c
leventepap <pap.levente.i@gmail.com>
2022-10-27 20:25:19 +0200

    new endpoint (hopefully)

--- backend/restaurant/urls.py
+++ backend/restaurant/urls.py
@@ -4,10 +4,12 @@ from restaurant.views import AllRestaurants
 from restaurant.views import NewRestaurant
 from restaurant.views import UserFilterRestaurant
 from restaurant.views import SingleRestaurant
+from restaurant.views import SelfRestaurants
 
 urlpatterns = [
     path('', AllRestaurants.as_view()),
     path('<int:id>/', SingleRestaurant.as_view()),
     path('user/<int:id>/', UserFilterRestaurant.as_view()),
-    path('new/', NewRestaurant.as_view())
+    path('new/', NewRestaurant.as_view()),
+    path('me/', SelfRestaurants.as_view()),
 ]
--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -53,3 +53,12 @@ class UserFilterRestaurant(ListAPIView):
 
     def get_queryset(self):
         return Restaurant.objects.filter(user=self.request.user)
+
+
+class SelfRestaurants(GenericAPIView):
+    queryset = ''
+
+    def get(self, request, *args, **kwargs):
+        instance = Restaurant.objects.filter(user=self.request.user)
+        serializer = RestaurantSerializer(instance, many=True)
+        return Response(serializer.data)
